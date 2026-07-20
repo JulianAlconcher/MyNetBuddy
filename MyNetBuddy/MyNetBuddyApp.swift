47a287db9d33af3605c610a3c82f172a9c5f4cb5
Julian Alconcher <111883752+JulianAlconcher@users.noreply.github.com>
2026-07-20 14:12:27 -0300

    Initial Commit

---
+++ MyNetBuddy/MyNetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNetBuddyApp.swift
+//  MyNetBuddy
+//
+//  Created by Julian Alconcher on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyNetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyNetBuddyTests/MyNetBuddyTests.swift
@@ -0,0 +1,18 @@
+//
+//  MyNetBuddyTests.swift
+//  MyNetBuddyTests
+//
+//  Created by Julian Alconcher on 20/07/2026.
+//
+
+import Testing
+
+struct MyNetBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
